418a131f352cc0887f046e703464bdf78f818c2f
Kent Andrew Ferrer <kentandrewferrer@Kents-Laptop.local>
2024-04-13 15:08:49 +0800

    first commit

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+
+window.addEventListener('load', function () {
+  var loadingScreen = document.getElementById('loading-screen');
+  var mainContent = document.getElementById('main-content');
+  
+  // Simulate preloading process (you can replace this with actual preloading)
+  setTimeout(function () {
+    // Hide loading screen
+    loadingScreen.style.display = 'none';
+    
+    // Show main content
+    mainContent.style.display = 'block';
+  }, 5000); // 5 seconds delay
+});
